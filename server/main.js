ce4833ce0b6282d7568ed86e17d83a2becd33565
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-24 22:25:04 +0530

    added vercel config for backedn

--- server/main.js
+++ server/main.js
@@ -12,7 +12,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
   res.header("Access-Control-Expose-Headers", "x-auth-token");
-  res.send("App is running")
   next();
 });
 
@@ -25,6 +24,11 @@ app.use('/bankingapp/api/account', accountservicerouter);
 app.use('/bankingapp/api/transaction', transactionservicerouter);
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('App is running');
+});
+
 
 if (environment === 'development') {
   app.use(morgan('tiny'));
