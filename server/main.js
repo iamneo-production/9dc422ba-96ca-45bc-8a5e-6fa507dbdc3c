2a8680b75eaa593a6a605f885a65e7e272e26a1d
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-01-24 22:20:31 +0530

    added vercel config for backedn

--- server/main.js
+++ server/main.js
@@ -12,6 +12,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
   res.header("Access-Control-Expose-Headers", "x-auth-token");
+  res.send("App is running")
   next();
 });
 
