02626ef6459115763a1ae32eddecb6b266957f93
Ayush Bhai Mehta <ayushbhaimehta20002@gmail.com>
2023-02-06 15:46:45 +0530

    Refactored MVC folder structure added

--- server/main.js
+++ server/main.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+require('dotenv').config()
 
-const environment = app.get('env');
+
+// console.log(app.get('env'));
+// set env
+const environment = process.env.NODE_ENV || "development";
+console.log({ environment });
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 
+const bodyParser = require('body-parser');
 
+// some basic header for auth
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
@@ -15,16 +22,23 @@ app.use(function (req, res, next) {
   next();
 });
 
-const userservicerouter = require('./user-service/user-controller');
-const accountservicerouter = require('./account-service/account-controller');
-const transactionservicerouter = require('./transaction-service/transaction-controller');
-const bodyParser = require('body-parser');
-app.use('/bankingapp/api/user', userservicerouter);
-app.use('/bankingapp/api/account', accountservicerouter);
-app.use('/bankingapp/api/transaction', transactionservicerouter);
+// -----------------> Routes <-----------------------------------//
+
+const userservicerouter = require('./controllers/user-controller');
+const accountservicerouter = require('./controllers/account-controller');
+const transactionservicerouter = require('./controllers/transaction-controller');
+
+// -----------------> Routes Setup <---------------------------------//
+app.use('/api/user', userservicerouter);
+app.use('/api/account', accountservicerouter);
+app.use('/api/transaction', transactionservicerouter);
+
+
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 
+
+// --------------------------> Checking for Deployment purposes <----------------------- // 
 app.get('/', (req, res) => {
   res.send('App is running');
 });
@@ -32,10 +46,11 @@ app.get('/', (req, res) => {
 
 if (environment === 'development') {
   app.use(morgan('tiny'));
+  // ------------------------> Logger (Morgan) <---------------------------- //
   console.log('Morgan is enabled...');
 }
 
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Application running in ${environment} environment, listening to port ${port}....`);
