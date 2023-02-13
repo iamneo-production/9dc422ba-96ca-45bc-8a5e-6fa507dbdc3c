87efbcf2a87cefda3dde5f775307d9d1d7b749dd
anujawas <awasthianuj394@gmail.com>
2023-02-14 02:31:11 +0530

    created test snippets

---
+++ client/src/components/__test__/Slider.test.js
@@ -0,0 +1,21 @@
+import {render, screen, cleanup} from "@testing-library/react"
+
+import Slideshow from "../HomeSlider"
+
+afterEach(()=>{
+    cleanup();
+})
+
+test("SlideShow Component should Render",()=>{
+    render(<Slideshow/>);
+    const SliderElement =screen.getByTestId("Slideshow");
+    expect(SliderElement).toBeInTheDocument();
+    expect(SliderElement).toHaveClass("slideshow")
+})
+
+test("SlideShow class in slideshow Component should render",()=>{
+    render(<Slideshow/>);
+    const SliderElement =screen.getAllByTestId("sliderimage");
+    expect(SliderElement[0]).toBeInTheDocument();
+    expect(SliderElement[0]).toHaveAttribute("alt")
+})
