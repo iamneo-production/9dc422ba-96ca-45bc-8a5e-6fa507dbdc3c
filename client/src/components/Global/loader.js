21d9572ee3067d7ddb57c534d2bd4045aa250ca4
anujawas <awasthianuj394@gmail.com>
2024-01-20 23:21:18 +0530

    Useloader hook

--- client/src/components/Global/loader.js
+++ client/src/components/Global/loader.js
@@ -1,9 +1,11 @@
 import { BeatLoader } from "react-spinners"
 import React from "react"
-export const Loader = ({loader}) => {
+import { useLoader } from "../../hooks/useLoader"
+export const Loader = () => {
+    const { loader } = useLoader()
     return (
-        <div style={{ zIndex: "10", position: "absolute", opacity: "50%", backgroundColor:"grey", width: "100%", height: "700px", display: loader}}>
-            <div style={{display: "flex", justifyContent: "center", alignItems: "center", width:"100%", height:"100%"}}>
+        <div style={{ zIndex: "999", position: "absolute", opacity: "50%", backgroundColor: "grey", width: "100%", height: "700px", display: loader ? "block" : "none" }}>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "100%", height: "100%" }}>
                 <BeatLoader color="red" size={30} />
             </div>
         </div>
