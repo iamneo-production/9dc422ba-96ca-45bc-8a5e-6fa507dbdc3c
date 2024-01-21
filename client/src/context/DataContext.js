4abe093abfe927c96dbda4535c24fc644b675a1a
anujawas <awasthianuj394@gmail.com>
2024-01-22 03:21:35 +0530

    income and expense state handelled

--- client/src/context/DataContext.js
+++ client/src/context/DataContext.js
@@ -4,18 +4,33 @@ export const DataContext = createContext({
     setUserData: null,
     accountData: null,
     setAccountData: null,
+    tData: null,
+    setTData: null,
+    income: null,
+    setIncome: null,
+    expense: null,
+    setExpense: null,
 })
 
 
 export const DataContextProvider = ({ children }) => {
     const [userData, setUserData] = useState(null);
     const [accountData, setAccountData] = useState(null);
+    const [tData, setTData] = useState([])
+    const [income, setIncome] = useState(0);
+    const [expense, setExpense] = useState(0);
 
     const value = {
         userData,
         setUserData,
         accountData,
-        setAccountData
+        setAccountData,
+        tData,
+        setTData,
+        expense,
+        setIncome,
+        income,
+        setExpense
     }
     return (
         <DataContext.Provider value={value}>
